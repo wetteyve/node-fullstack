d810c137c33ba7f21dccafd0e9d12f5a30eaad52
wetteyve <yveswetter@gmx.ch>
2025-05-19 14:26:46 +0200

    implement navigation

--- 911rs/components/footer/footer.tsx
+++ 911rs/components/footer/footer.tsx
@@ -4,11 +4,11 @@ const Footer = ({footerEntries}: {footerEntries: any}) => (
     <div className='z-10 min-h-24 bg-primary text-white'>
       <div className='app-container h-full'>
         <div className='flex h-full flex-col-reverse items-center justify-between gap-4 md:flex-row'>
-          <span className='r-text-xs my-auto'>{`© ${new Date().getFullYear()} - 911 RS - Alte 11er Garage Arbon`}</span>
+          <span className='typo-headline-xs my-auto'>{`© ${new Date().getFullYear()} - 911 RS - Alte 11er Garage Arbon`}</span>
           <div className='my-auto flex flex-wrap justify-between gap-6 md:gap-14 xl:gap-28'>
             {footerEntries.map((e: any, i: any) => (
               <Link key={i} to={e.slug}>
-                <span className='r-text-xs hover:cursor-pointer'>{` ${e.navigation_title}`}</span>
+                <span className='typo-headline-xs hover:cursor-pointer'>{` ${e.navigation_title}`}</span>
               </Link>
             ))}
           </div>
--- 911rs/components/navbar/navbar.tsx
+++ 911rs/components/navbar/navbar.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { Link, useLocation } from 'react-router';
+import { NavLink, useLocation } from 'react-router';
 import MobileNavbar from './mobile-navbar';
 
 type NavbarProps = {
@@ -35,21 +35,27 @@ const Navbar = ({navbarEntries, footerEntries}: NavbarProps) => {
     <div className={`sticky z-20 bg-white shadow-md ${visible && 'top-0 motion-safe:animate-fadeIn md:motion-safe:animate-fadeInLight'}`}>
       <div className='container mx-auto flex h-24 justify-between px-5 py-2'>
         <div className='flex'>
-          <Link to={`./${navbarEntries[0]?.slug}`} className='my-auto mr-5 flex flex-col items-center hover:cursor-pointer'>
-            <p className='r-text-xl whitespace-nowrap font-semibold leading-none text-primary'>911 RS</p>
-            <p className='r-text-xs whitespace-nowrap font-semibold leading-none'>ALTE 11ER GARAGE</p>
-            <p className='r-text-xs font-light leading-normal'>ARBON</p>
-          </Link>
+          <NavLink to={`./${navbarEntries[0]?.slug}`} className='my-auto mr-5 flex flex-col items-center hover:cursor-pointer'>
+            <p className='whitespace-nowrap text-primary typo-headline-xl'>911 RS</p>
+            <p className='whitespace-nowrap typo-display-sm font-bold'>ALTE 11ER GARAGE</p>
+            <p className='typo-display-sm'>ARBON</p>
+          </NavLink>
         </div>
         <div className='hidden md:flex'>
           {navbarEntriesWithoutFirst.map((e:any, i:any) => (
-            <Link key={i} className='mb-6 ml-12 mt-auto hover:cursor-pointer' to={e.slug}>
+            <NavLink 
+              key={i}
+              to={e.slug}
+              className={({ isActive }) =>
+                  `mb-6 ml-12 mt-auto hover:cursor-pointer${isActive ? " text-primary" : ""}`
+                }
+            >
               <p
-                className={`r-text-s font-light transition-all duration-150 ease-in hover:scale-105 ${pathname === e.slug && 'text-primary'}`}
+                className={`typo-headline-md transition-all duration-150 ease-in hover:scale-105 ${pathname === e.slug && 'text-primary'}`}
               >
                 {e.navigation_title.toUpperCase()}
               </p>
-            </Link>
+            </NavLink>
           ))}
         </div>
         <div className='block md:hidden'>
