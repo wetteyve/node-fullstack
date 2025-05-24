437ff09b76eb0d5320c634bb941827b36df7c7f7
wetteyve <yveswetter@gmx.ch>
2025-05-24 12:57:40 +0200

    change about page breakpoint

--- 911rs/pages/About.tsx
+++ 911rs/pages/About.tsx
@@ -1,20 +1,27 @@
 import { HeaderPicture } from '#rs911/components/building-blocks/image/HeaderPicture';
 import { Image } from '#rs911/components/building-blocks/image/Image';
 import { Lead } from '#rs911/components/building-blocks/Lead';
-import { useScreenStore } from '#rs911/store/screen.store';
+import { type ScreenSize, useScreenStore } from '#rs911/store/screen.store';
 import { type AboutContent } from '#rs911/utils/strapi.utils';
+
+// this screen sizes are considered small
+const smallSceenSize: ScreenSize[] = ['sm', 'md'];
 
 export const About = ({ content: { about, car } }: { content: AboutContent }) => {
   const screenSize = useScreenStore.use.screenSize();
-  const AboutImage =
-    screenSize === 'sm' ? <HeaderPicture file={about.picture} /> : <Image file={about.picture} twAspect='w-[36%] !object-contain' />;
+  const isSmallScreen = smallSceenSize.includes(screenSize);
+  const AboutImage = isSmallScreen ? (
+    <HeaderPicture file={about.picture} />
+  ) : (
+    <Image file={about.picture} twAspect='w-[36%] !object-contain' />
+  );
   const AboutTitle = <h1 className='typo-headline-lg mt-8'>{about.title}</h1>;
   const AboutDescription = <Lead lead={{ title: '', description: about.description }} className='pb-12' />;
 
   return (
     <>
       <section>
-        {screenSize === 'sm' ? (
+        {isSmallScreen ? (
           <>
             {AboutImage}
             {AboutTitle}
