f87a6fbf95c7dff94e88730d6a31aabeb0f44c7e
wetteyve <yveswetter@gmx.ch>
2025-05-27 21:51:52 +0200

    implement sitemap

---
+++ app/routes/[new.uht-herisau]/[sitemap.xml]/index.tsx
@@ -0,0 +1,26 @@
+import { fetchStrapiPages } from '#uht-herisau/utils/page.utils';
+import { type Route } from '../+types';
+
+export const loader = async ({ request, context: { tenant } }: Route.LoaderArgs) => {
+  const pages = await fetchStrapiPages();
+  const { pathname, host } = new URL(request.url);
+  const cleanPathname = tenant ? pathname.replace(`/${tenant}`, '') : pathname;
+  const locations = Object.keys(pages)
+    .map((slug) => {
+      const fullUrl = `${ENV.MODE === 'production' ? 'https' : 'http'}://${host}${cleanPathname.replace('/sitemap.xml', `/${slug}`)}`;
+      return `<url><loc>${fullUrl}</loc></url>`;
+    })
+    .join();
+
+  const body = `
+  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:news="http://www.google.com/schemas/sitemap-news/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml" xmlns:mobile="http://www.google.com/schemas/sitemap-mobile/1.0" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1" xmlns:video="http://www.google.com/schemas/sitemap-video/1.1">
+  ${locations}
+  </urlset>`;
+
+  return new Response(body, {
+    headers: {
+      'Content-Type': 'application/xml',
+      'Cache-Control': 'public, max-age=3600', // Cache for 1 hour
+    },
+  });
+};
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,19 +1,23 @@
 import { type LoaderFunctionArgs } from 'react-router';
-import { fetchStrapiPages } from '#rs911/utils/page.utils';
+import { fetchStrapiPages as fetch911rsPages } from '#rs911/utils/page.utils';
+import { fetchStrapiPages as fetchUhtPages } from '#uht-herisau/utils/page.utils';
 
 export const loader = async ({ request }: LoaderFunctionArgs) => {
   if (ENV.MODE === 'development') {
-    const rs911Pages = await fetchStrapiPages();
-    const rs9111links = Object.values(rs911Pages).reduce((acc: { [key: string]: string }, page) => {
+    const rs911Pages = await fetch911rsPages();
+    const rs911links = Object.values(rs911Pages).reduce((acc: { [key: string]: string }, page) => {
       acc[page.slug] = `${request.url}911rs.yveswetter/${page.slug}`;
+      return acc;
+    }, {});
+    const uhtPages = await fetchUhtPages();
+    const uhtlinks = Object.values(uhtPages).reduce((acc: { [key: string]: string }, page) => {
+      acc[page.path] = `${request.url}new.uht-herisau/${page.path}`;
       return acc;
     }, {});
 
     return {
-      '911rs': { ...rs9111links, sitemap: `${request.url}911rs.yveswetter/sitemap.xml` },
-      'uht-herisau': {
-        home: `${request.url}new.uht-herisau/home`,
-      },
+      '911rs': { ...rs911links, sitemap: `${request.url}911rs.yveswetter/sitemap.xml` },
+      'uht-herisau': { ...uhtlinks, sitemap: `${request.url}new.uht-herisau/sitemap.xml` },
     };
   }
 };
