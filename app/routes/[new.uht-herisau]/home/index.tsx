1f4cf464ff592ebd0d57701ec3db39637986081a
wetteyve <yveswetter@gmx.ch>
2025-05-27 14:03:48 +0200

    implement animations of uht-herisau

--- app/routes/[new.uht-herisau]/home/index.tsx
+++ app/routes/[new.uht-herisau]/home/index.tsx
@@ -1,5 +1,9 @@
 const App = () => {
-  return <div className='typo-xl'>uht-herisau page</div>;
+  return (
+    <div className='typo-xl'>
+      <p className='animate-fade-in'>uht-herisau page</p>
+    </div>
+  );
 };
 
 export default App;
