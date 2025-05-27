77e01ed549d8a6b7e56c93b3f75fdd3b5a159111
wetteyve <yveswetter@gmx.ch>
2025-05-27 14:57:22 +0200

    hide overflow on 911rs

--- app/routes/[911rs.yveswetter]/index.tsx
+++ app/routes/[911rs.yveswetter]/index.tsx
@@ -95,7 +95,7 @@ const Page = ({ loaderData: { navbarEntries, footerEntries } }: Route.ComponentP
   }, [updateScreenSize]);
 
   return (
-    <div className='relative'>
+    <div className='relative overflow-x-hidden'>
       <Navbar navbarEntries={navbarEntries} />
       <LoadingBar />
       <div className='app-container !pb-0 min-h-[calc(100svh-96px-96px)]'>
