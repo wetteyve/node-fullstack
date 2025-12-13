67df23732dd245c2f8671eb375da301403c24d06
wetteyve <yveswetter@gmx.ch>
2025-12-13 14:47:00 +0100

    feature: load WASM in clientLoader

--- app/routes/scheduler.yveswetter/home.tsx
+++ app/routes/scheduler.yveswetter/home.tsx
@@ -1,18 +1,42 @@
-import { useClientOnlyModule } from '#app/utils/client-only-module.utils';
+import { resourceBase } from '#app/utils/app-paths';
+import { getGlobalMetaTags } from '#app/utils/meta.utils';
+import { type Route } from './+types/home';
 
 const arr = [{}, 1, 'hello', true];
 
-const App = () => {
-  const wasm = useClientOnlyModule(() => import('@wetteyve/scheduler'));
+// SSR Metadata
+export const loader = async () => {
+  const resourceBasePath = ENV.MODE !== 'development' ? resourceBase : '';
+  const faviconUrl = `${resourceBasePath}/favicon.ico`;
+  return {
+    meta: [
+      ...getGlobalMetaTags(),
+      {
+        tagname: 'link',
+        rel: 'icon',
+        href: faviconUrl,
+      },
+    ],
+  };
+};
 
-  const handleClick = () => window.alert(`Hello from napi-rs: ${wasm?.getArrayLength(arr)}`);
+// Setup WASM in client browser
+export const clientLoader = async ({ serverLoader }: Route.ClientLoaderArgs) => {
+  const [serverData, wasm] = await Promise.all([serverLoader(), import('@wetteyve/scheduler')]);
+  return { ...serverData, wasm };
+};
+clientLoader.hydrate = true as const;
+
+const App = ({ loaderData: { wasm } }: Route.ComponentProps) => {
+  const handleClick = () => window.alert(`Hello from napi-rs: ${wasm.getArrayLength(arr)}`);
+
   return (
     <div>
-      <button disabled={!wasm} onClick={handleClick}>
-        {wasm ? 'PUSH FOR WASM!' : 'Loading...'}
-      </button>
+      <button onClick={handleClick}>PUSH FOR WASM!</button>
     </div>
   );
 };
 
 export default App;
+
+export const HydrateFallback = () => <p>Loading...</p>;
